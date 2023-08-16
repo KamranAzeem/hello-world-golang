eea33f106b6ba10eb2368239dd1818606dd121fa
Kamran Azeem <kamranazeem@gmail.com>
2023-08-16 14:59:02 +0200

    changed color to orange

--- src/hello-world.go
+++ src/hello-world.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello world - We like: black")
+    fmt.Fprintf(w, "Hello world - We like: orange")
 }
